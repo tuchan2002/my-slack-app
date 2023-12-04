f6034303651f14416be2ce7f2857b5b1a45bc436
minamoto <truongkhoi31052002@gmail.com>
2023-12-04 12:02:43 +0700

    update channel list when adding new message

--- src/redux/actions/messageAction.js
+++ src/redux/actions/messageAction.js
@@ -6,7 +6,7 @@ import {
     where
 } from 'firebase/firestore';
 import { db } from '../../firebase/config';
-import { GET_REALTIME_MESSAGES_BY_CHANNEL } from '../types';
+import { GET_REALTIME_MESSAGES_BY_CHANNEL, PUSH_CHANNEL_ON_TOP } from '../types';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getRealtimeMessagesByChannel = (data) => (dispatch) => {
@@ -26,6 +26,30 @@ export const getRealtimeMessagesByChannel = (data) => (dispatch) => {
             type: GET_REALTIME_MESSAGES_BY_CHANNEL,
             payload: documents
         });
+    });
+
+    return unsubscribe;
+};
+
+/**
+ * Listen to the Messages table on Firebase. Update message states and channel states when the database changed.
+ */
+export const listenRealtimeMessages = () => (dispatch) => {
+    const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+        // TODO: update messages
+
+        // Push channel of the latest message on top of channel list.
+        if (snapshot.size > 0) {
+            const lastMessage = snapshot.docs[0];
+            dispatch({
+                type: PUSH_CHANNEL_ON_TOP,
+                payload: {
+                    channelId: lastMessage.data().channelId
+                }
+            });
+        }
     });
 
     return unsubscribe;
--- src/redux/reducers/channelReducer.js
+++ src/redux/reducers/channelReducer.js
@@ -1,7 +1,8 @@
 import {
     GET_REALTIME_CHANNELS,
     GET_REALTIME_MEMBERS,
-    SELECT_CHANNEL
+    SELECT_CHANNEL,
+    PUSH_CHANNEL_ON_TOP
 } from '../types';
 
 const initialState = {
@@ -9,6 +10,38 @@ const initialState = {
     selectedChannel: null,
     members: []
 };
+
+/**
+ * Push a channel to the front of the channel state (array), given its id.
+ *
+ * This function does not modify the original state, it returns a copy of the new state.
+ *
+ * @param state - Current redux state
+ * @param{number} channelId - ID of channel to be pushed on top
+ *
+ * @returns new redux state after pushing targeted channel on top
+ */
+function pushChannelOnTop(state, channelId) {
+    const newState = {...state};
+    const targetChannelId = newState.channels.findIndex(
+        (channel) => channel.id === channelId
+    );
+
+    // If the channel is already the first channel of the list, it does not need to be pushed on top.
+    if (targetChannelId <= 0) {
+        return state;
+    }
+
+    if (newState.selectedChannel === targetChannelId) {
+        newState.selectedChannel = 0;
+    }
+
+    const targetChannel = newState.channels[targetChannelId];
+    newState.channels.splice(targetChannelId, 1);
+    newState.channels.unshift(targetChannel);
+
+    return newState;
+}
 
 const channelReducer = (state = initialState, action = {}) => {
     switch (action.type) {
@@ -33,6 +66,8 @@ const channelReducer = (state = initialState, action = {}) => {
             ...state,
             members: action.payload
         };
+    case PUSH_CHANNEL_ON_TOP:
+        return pushChannelOnTop(state, action.payload.channelId);
     default:
         return state;
     }
